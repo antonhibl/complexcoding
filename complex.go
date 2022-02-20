e81f970db41e4bbf7ca76f106c71735cd5da1dc2
Anton Hibl <antonhibl11@gmail.com>
2022-02-20 01:05:59 -0700

    added the start of a matrix structure and basic generator/display functions

--- complex.go
+++ complex.go
@@ -6,6 +6,13 @@ import (
 	"strconv"
 )
 
+type quantum_gate struct {
+	r1c1 complex128
+	r1c2 complex128
+	r2c1 complex128
+	r2c2 complex128
+}
+
 func complex_add(x complex128, y complex128) (result complex128) {
 	result = x + y
 	return result
@@ -16,21 +23,45 @@ func complex_multiply(x complex128, y complex128) (result complex128) {
 	return result
 }
 
+func complex_divide(x complex128, y complex128) (result complex128) {
+	result = x / y
+	return result
+}
+
+func matrix_maker(r1c1 complex128, r1c2 complex128, r2c1 complex128, r2c2 complex128) (result quantum_gate) {
+	result = quantum_gate{
+		r1c1: r1c1,
+		r1c2: r2c2,
+		r2c1: r2c1,
+		r2c2: r2c2,
+	}
+	return result
+}
+
+func matrix_display(matrix quantum_gate) {
+	fmt.Print("[ ", matrix.r1c1)
+	fmt.Print(" ", matrix.r1c2)
+	fmt.Print(" ]\n")
+	fmt.Print("[ ", matrix.r2c1)
+	fmt.Print(" ", matrix.r2c2)
+	fmt.Print(" ]\n")
+}
+
 func main() {
 	// inputs for complex X
 	// Real
-	x1, _ := strconv.Atoi(os.Args[1])
+	x1, _ := strconv.ParseFloat(os.Args[1], 128)
 	// Imaginary
-	x2, _ := strconv.Atoi(os.Args[2])
+	x2, _ := strconv.ParseFloat(os.Args[2], 128)
 	// complex = real + imaginary
 	var x complex128
 	x = complex(float64(x1), float64(x2))
 
 	// inputs for complex Y
 	// Real
-	y1, _ := strconv.Atoi(os.Args[3])
+	y1, _ := strconv.ParseFloat(os.Args[3], 128)
 	// Imaginary
-	y2, _ := strconv.Atoi(os.Args[4])
+	y2, _ := strconv.ParseFloat(os.Args[4], 128)
 	// complex = real + imaginary
 	var y complex128
 	y = complex(float64(y1), float64(y2))
@@ -56,4 +87,8 @@ func main() {
 	fmt.Print("\nX / Y = ", x/y)
 	// y/x
 	fmt.Print("\nY / X = ", y/x)
+	fmt.Print("\n\n")
+
+	q := matrix_maker(x, -x, y, -y)
+	matrix_display(q)
 }
